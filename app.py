78435391fd0bb05f1cd5f8488db67fa183c0ff0e
pypeaday <pypeaday@pype.dev>
2024-12-09 09:41:59 -0600

    better default port

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import uvicorn
 import os
 
-# Get port from environment variable, default to 8002
-port = int(os.getenv('SOONISH_PORT', '8002'))
+# Get port from environment variable, default to 8000
+port = int(os.getenv('SOONISH_PORT', '8000'))
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
